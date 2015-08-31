0471e21ee057d84726fb4d3163c6f692ca09b0e8
Ashley Sullins & Annie Brown <student@epicodus.com>
2015-08-31 10:30:59 -0700

    Add passing tests for new categories_tasks table

--- src/test/java/CategoryTest.java
+++ src/test/java/CategoryTest.java
@@ -1,6 +1,7 @@
 import org.junit.*;
 import static org.junit.Assert.*;
 import java.util.Arrays;
+import java.util.List;
 
 public class CategoryTest {
 
@@ -33,7 +34,34 @@ public class CategoryTest {
     Category savedCategory = Category.find(myCategory.getId());
     assertTrue(myCategory.equals(savedCategory));
   }
-  
+
+  @Test
+  public void addTask_addsTaskToCategory() {
+    Category myCategory = new Category("Household chores");
+    myCategory.save();
+
+  Task myTask = new Task("Mow the lawn");
+  myTask.save();
+
+  myCategory.addTask(myTask);
+  Task savedTask = myCategory.getTasks().get(0);
+  assertTrue(myTask.equals(savedTask));
+ }
+
+ @Test
+ public void getTasks_returnsAllTasks_ArrayList() {
+   Category myCategory = new Category("Household chores");
+   myCategory.save();
+
+   Task myTask = new Task("Mow the lawn");
+   myTask.save();
+
+   myCategory.addTask(myTask);
+   List savedTasks = myCategory.getTasks();
+   assertEquals(savedTasks.size(), 1);
+ }
+
+
   // @Test
   // public void getTasks_retrievesALlTasksFromDatabase_tasksList() {
   //   Category myCategory = new Category("Household chores");
